fda7f5315e6f2b2f47b1bf166f7e26e17a89974b
asadfgglie <ac121878@outlook.com>
2026-04-30 14:40:16 +0800

    v1.0.5-dev

--- test/main_hw4.c
+++ test/main_hw4.c
@@ -39,24 +39,23 @@ void generate_random_matrix(Matrix *A, int const row, int const col, int target_
     A->indices = (MKL_INT *)malloc(target_nnz * sizeof(MKL_INT));
     A->is_csr = is_csr;
 
-    int const dim = is_csr ? row : col;
-    A->ptr = (MKL_INT *)calloc(dim + 1, sizeof(MKL_INT));
+    int const compressed_dim = is_csr ? row : col, other_dim = is_csr ? col : row;
+    A->ptr = (MKL_INT *)calloc(compressed_dim + 1, sizeof(MKL_INT));
 
     // Simple random distribution of nnz across rows or cols
-    int const nnz_per_cr = target_nnz / dim;
-    int const remainder = target_nnz % dim;
+    int const nnz_per_cr = target_nnz / compressed_dim, remainder = target_nnz % compressed_dim;
     int current_nnz = 0;
-    int *usages = malloc(dim * sizeof(int));
+    int *usages = malloc(other_dim * sizeof(int));
     if (is_csr) {
         for (int i = 0; i < row; i++) {
             A->ptr[i] = current_nnz;
             int const row_nnz = nnz_per_cr + (i < remainder ? 1 : 0);
-            memset(usages, 0, dim * sizeof(int));
+            memset(usages, 0, other_dim * sizeof(int));
             for (int j = 0; j < row_nnz; j++) {
-                A->values[current_nnz] = rand() / (float)RAND_MAX;
-                int index = rand() % dim;
+                A->values[current_nnz] = (float)rand() / (float)RAND_MAX;
+                int index = rand() % other_dim;
                 while (usages[index] > 0) {
-                    index = rand() % dim;
+                    index = rand() % other_dim;
                 }
                 usages[index]++;
                 A->indices[current_nnz] = index;
@@ -68,12 +67,12 @@ void generate_random_matrix(Matrix *A, int const row, int const col, int target_
         for (int j = 0; j < col; j++) {
             A->ptr[j] = current_nnz;
             int const col_nnz = nnz_per_cr + (j < remainder ? 1 : 0);
-            memset(usages, 0, dim * sizeof(int));
+            memset(usages, 0, other_dim * sizeof(int));
             for (int i = 0; i < col_nnz; i++) {
-                A->values[current_nnz] = rand() / (float)RAND_MAX;
-                int index = rand() % dim;
+                A->values[current_nnz] = (float)rand() / (float)RAND_MAX;
+                int index = rand() % other_dim;
                 while (usages[index] > 0) {
-                    index = rand() % dim;
+                    index = rand() % other_dim;
                 }
                 usages[index]++;
                 A->indices[current_nnz] = index;
@@ -81,38 +80,41 @@ void generate_random_matrix(Matrix *A, int const row, int const col, int target_
             }
         }
     }
-    A->ptr[dim] = current_nnz;
+    A->ptr[compressed_dim] = current_nnz;
 
     free(usages);
 }
-void free_matrix(Matrix *A) {
+void free_matrix(Matrix const *A) {
     free(A->values);
     free(A->indices);
     free(A->ptr);
 }
 void generate_dense(float *mat, int const row, int const col) {
+    if (mat == NULL) {
+        return;
+    }
     for (int i = 0; i < row * col; i++) {
-        mat[i] = rand() / (float)RAND_MAX;
+        mat[i] = (float)rand() / (float)RAND_MAX;
     }
 }
-void run_benchmark(int m, int n_A, int k, int use_csr) {
-    int target_nnz = 32 * m;
+void run_benchmark(int const m, int const n, int const k, int const use_csr) {
+    int const target_nnz = 32 * m;
     printf("\nBenchmarking: A(%d x %d), B(%d x %d), target_nnz=%d, format=%s\n", 
-           m, n_A, n_A, k, target_nnz, use_csr ? "CSR" : "CSC");
-    float *B = malloc(n_A * k * sizeof(float));
+           m, n, n, k, target_nnz, use_csr ? "CSR" : "CSC");
+    float *B = malloc(n * k * sizeof(float));
     float *C_mkl = malloc(m * k * sizeof(float));
     float *C_custom = malloc(m * k * sizeof(float));
-    generate_dense(B, n_A, k);
+    generate_dense(B, n, k);
     double start, end;
     Matrix A;
+    generate_random_matrix(&A, m, n, target_nnz, use_csr);
+    sparse_matrix_t mkl_A;
+    struct matrix_descr descr;
+    descr.type = SPARSE_MATRIX_TYPE_GENERAL;
     if (use_csr) {
-        generate_random_matrix(&A, m, n_A, target_nnz, 1);
         // MKL CSR execution
-        sparse_matrix_t mkl_A;
         mkl_sparse_s_create_csr(&mkl_A, SPARSE_INDEX_BASE_ZERO, A.m, A.n, 
                                 A.ptr, A.ptr + 1, A.indices, A.values);
-        struct matrix_descr descr;
-        descr.type = SPARSE_MATRIX_TYPE_GENERAL;
         mkl_sparse_optimize(mkl_A);
         start = get_time();
         // https://www.intel.com/content/www/us/en/docs/onemkl/developer-reference-c/2025-2/mkl-sparse-mm.html
@@ -128,13 +130,9 @@ void run_benchmark(int m, int n_A, int k, int use_csr) {
         printf("Custom CSR Time: %f s\n", end - start);
         free_matrix(&A);
     } else {
-        generate_random_matrix(&A, m, n_A, target_nnz, 0);
         // MKL CSC execution
-        sparse_matrix_t mkl_A;
         mkl_sparse_s_create_csc(&mkl_A, SPARSE_INDEX_BASE_ZERO, A.m, A.n, 
                                 A.ptr, A.ptr + 1, A.indices, A.values);
-        struct matrix_descr descr;
-        descr.type = SPARSE_MATRIX_TYPE_GENERAL;
         mkl_sparse_optimize(mkl_A);
         start = get_time();
         mkl_sparse_s_mm(SPARSE_OPERATION_NON_TRANSPOSE, 1.0f, mkl_A, descr, 
@@ -155,25 +153,22 @@ void run_benchmark(int m, int n_A, int k, int use_csr) {
 }
 int main() {
     srand(time(NULL));
+    int const n_vals[] = {2048, 32768}, k_vals[] = {64, 512};
     for (int i = 0; i < 2; i++) {
-        const int n_vals[] = {2048, 32768};
-        const int n = n_vals[i];
         for (int j = 0; j < 2; j++) {
-            const int k_vals[] = {64, 512};
-            const int k = k_vals[j];
             // Test 3 shapes for A: (n,n), (4n,n), (n,4n)
-            const int shapes[3][2] = {
-                {n, n},
-                {4*n, n},
-                {n, 4*n}
+            int const shapes[3][2] = {
+                {n_vals[i], n_vals[i]},
+                {4 * n_vals[i], n_vals[i]},
+                {n_vals[i], 4 * n_vals[i]}
             };
             for (int s = 0; s < 3; s++) {
-                const int m = shapes[s][0];
-                const int n_A = shapes[s][1];
+                int const m = shapes[s][0];
+                int const n = shapes[s][1];
                 // CSR Format
-                run_benchmark(m, n_A, k, 1);
+                run_benchmark(m, n, k_vals[j], 1);
                 // CSC Format
-                run_benchmark(m, n_A, k, 0);
+                run_benchmark(m, n, k_vals[j], 0);
             }
         }
     }
