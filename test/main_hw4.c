9cbf7d9b7e6b5c3e2d599b65e781c4a82dc6c795
asadfgglie <ac121878@outlook.com>
2026-05-01 15:31:09 +0800

    v1.0.10-dev

--- test/main_hw4.c
+++ test/main_hw4.c
@@ -4,56 +4,51 @@
 #include <mkl.h>
 #include <string.h>
 #include <sys/time.h>
+#ifdef USE_OMP
+#include <omp.h>
+#endif
 typedef struct { // CSR/CSC format structure
-    MKL_INT m; // row
-    MKL_INT n; // col
-    MKL_INT nnz;
-    float *values;
-    MKL_INT *indices;
-    MKL_INT *ptr;
-    int is_csr; // 1 if is csr, 0 if is csc
+    MKL_INT m, n, nnz; /* row, col, nnz */ float *values;
+    MKL_INT *indices, *ptr; int is_csr; // 1 if is csr, 0 if is csc
 } Matrix;
 double get_time() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
+    struct timeval tv; gettimeofday(&tv, NULL);
     return (double)tv.tv_sec + (double)tv.tv_usec * 1e-6;
 }
 void custom_mm(const Matrix *A, const float *B, float *C, MKL_INT const k, int const use_csr) {
     memset(C, 0, A->m * k * sizeof(float));
     if (use_csr) {
+#ifdef USE_OMP
+        #pragma omp parallel for default(none) shared(A, B, C, k) schedule(static)
+#endif
         for (MKL_INT i = 0; i < A->m; i++) {
             for (MKL_INT l = A->ptr[i]; l < A->ptr[i + 1]; l++) {
-                MKL_INT const a_col = A->indices[l];
-                float const a_val = A->values[l];
-                for (MKL_INT j = 0; j < k; j++) {
-                    C[i * k + j] += a_val * B[a_col * k + j];
-                }
+                MKL_INT const a_col = A->indices[l]; float const a_val = A->values[l];
+#ifdef USE_OMP
+                #pragma omp simd
+#endif
+                for (MKL_INT j = 0; j < k; j++) { C[i * k + j] += a_val * B[a_col * k + j]; }
             }
         }
     }
     else {
         for (MKL_INT i = 0; i < A->n; i++) {
             for (MKL_INT l = A->ptr[i]; l < A->ptr[i + 1]; l++) {
-                MKL_INT const a_row = A->indices[l];
-                float const a_val = A->values[l];
-                for (MKL_INT j = 0; j < k; j++) {
-                    C[a_row * k + j] += a_val * B[i * k + j];
-                }
+                MKL_INT const a_row = A->indices[l]; float const a_val = A->values[l];
+                for (MKL_INT j = 0; j < k; j++) { C[a_row * k + j] += a_val * B[i * k + j]; }
             }
         }
     }
 }
+int cmp (const void * a, const void * b) { return *(MKL_INT*)a > *(MKL_INT*)b; }
 void generate_random_matrix(Matrix *A, MKL_INT const row, MKL_INT const col, MKL_INT target_nnz, int const is_csr) {
     // ensure target_nnz is reasonable
     target_nnz = target_nnz > row * col ? row * col : target_nnz;
-    A->m = row;
-    A->n = col;
-    A->nnz = target_nnz;
-    A->values = (float *)malloc(target_nnz * sizeof(float));
-    A->indices = (MKL_INT *)malloc(target_nnz * sizeof(MKL_INT));
-    A->is_csr = is_csr;
+    A->m = row; A->n = col; A->nnz = target_nnz; A->is_csr = is_csr;
+    A->values = (float *)mkl_malloc(target_nnz * sizeof(float), 64);
+    A->indices = (MKL_INT *)mkl_malloc(target_nnz * sizeof(MKL_INT), 64);
     MKL_INT const compressed_dim = is_csr ? row : col, other_dim = is_csr ? col : row;
-    A->ptr = (MKL_INT *)calloc(compressed_dim + 1, sizeof(MKL_INT));
+    A->ptr = (MKL_INT *)mkl_malloc((compressed_dim + 1) * sizeof(MKL_INT), 64);
     MKL_INT const nnz_per_cr = target_nnz / compressed_dim, remainder = target_nnz % compressed_dim;
     MKL_INT current_nnz = 0;
     MKL_INT *usages = malloc(other_dim * sizeof(MKL_INT));
@@ -62,16 +57,14 @@ void generate_random_matrix(Matrix *A, MKL_INT const row, MKL_INT const col, MKL
             A->ptr[i] = current_nnz;
             MKL_INT const row_nnz = nnz_per_cr + (i < remainder ? 1 : 0);
             memset(usages, 0, other_dim * sizeof(MKL_INT));
+            MKL_INT const current_start = current_nnz;
             for (MKL_INT j = 0; j < row_nnz; j++) {
                 A->values[current_nnz] = (float)rand() / (float)RAND_MAX;
                 MKL_INT index = rand() % other_dim;
-                while (usages[index] > 0) {
-                    index = rand() % other_dim;
-                }
-                usages[index]++;
-                A->indices[current_nnz] = index;
-                current_nnz++;
+                while (usages[index] > 0) { index = rand() % other_dim; }
+                usages[index]++; A->indices[current_nnz] = index; current_nnz++;
             }
+            qsort(A->indices + current_start, row_nnz, sizeof(MKL_INT), cmp);
         }
     }
     else {
@@ -79,36 +72,26 @@ void generate_random_matrix(Matrix *A, MKL_INT const row, MKL_INT const col, MKL
             A->ptr[j] = current_nnz;
             MKL_INT const col_nnz = nnz_per_cr + (j < remainder ? 1 : 0);
             memset(usages, 0, other_dim * sizeof(MKL_INT));
+            MKL_INT const current_start = current_nnz;
             for (MKL_INT i = 0; i < col_nnz; i++) {
                 A->values[current_nnz] = (float)rand() / (float)RAND_MAX;
                 MKL_INT index = rand() % other_dim;
-                while (usages[index] > 0) {
-                    index = rand() % other_dim;
-                }
-                usages[index]++;
-                A->indices[current_nnz] = index;
-                current_nnz++;
+                while (usages[index] > 0) { index = rand() % other_dim; }
+                usages[index]++; A->indices[current_nnz] = index; current_nnz++;
             }
+            qsort(A->indices + current_start, col_nnz, sizeof(MKL_INT), cmp);
         }
     }
     A->ptr[compressed_dim] = current_nnz;
     free(usages);
 }
-void free_matrix(Matrix const *A) {
-    free(A->values);
-    free(A->indices);
-    free(A->ptr);
-}
+void free_matrix(Matrix const *A) { mkl_free(A->values); mkl_free(A->indices); mkl_free(A->ptr); }
 void generate_dense(float *mat, MKL_INT const row, MKL_INT const col) {
-    for (MKL_INT i = 0; i < row * col; i++) {
-        mat[i] = (float)rand() / (float)RAND_MAX;
-    }
+    for (MKL_INT i = 0; i < row * col; i++) { mat[i] = (float)rand() / (float)RAND_MAX; }
 }
 void error_check(float const *A, float const *B, MKL_INT const row, MKL_INT const col, float const tol) {
-    MKL_INT const N = row * col;
-    float *diff = malloc(N * sizeof(float));
-    cblas_scopy(N, A, 1, diff, 1);
-    cblas_saxpy(N, -1.0f, B, 1, diff, 1);
+    MKL_INT const N = row * col; float *diff = malloc(N * sizeof(float));
+    cblas_scopy(N, A, 1, diff, 1); cblas_saxpy(N, -1.0f, B, 1, diff, 1);
     float const abs_err = cblas_snrm2(N, diff, 1), rel_err = abs_err / cblas_snrm2(N, A, 1);
     if (rel_err > tol) {
         printf("Error check FAILED: Rel err %e (Abs: %e) exceeds tol %e\n", rel_err, abs_err, tol);
@@ -169,11 +152,11 @@ int main() {
     srand(time(NULL));
     MKL_INT const n_vals[] = {2048, 32768}, k_vals[] = {64, 512};
     for (MKL_INT i = 0; i < 2; i++) {
+        MKL_INT const shapes[3][2] = {
+            {n_vals[i], n_vals[i]}, {4 * n_vals[i], n_vals[i]},
+            {n_vals[i], 4 * n_vals[i]}
+        };
         for (MKL_INT j = 0; j < 2; j++) {
-            MKL_INT const shapes[3][2] = {
-                {n_vals[i], n_vals[i]}, {4 * n_vals[i], n_vals[i]},
-                {n_vals[i], 4 * n_vals[i]}
-            };
             for (MKL_INT s = 0; s < 3; s++) {
                 run_benchmark(shapes[s][0], shapes[s][1], k_vals[j], 1);
                 run_benchmark(shapes[s][0], shapes[s][1], k_vals[j], 0);
